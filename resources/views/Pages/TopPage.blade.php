da22450b191989a9b8e8807696cd21b65962ded1
Yu357 <forestgreen14689@gmail.com>
2022-08-26 17:12:27 +0900

    feat: TopPage用のCSSファイルを作成

--- resources/views/Pages/TopPage.blade.php
+++ resources/views/Pages/TopPage.blade.php
@@ -6,24 +6,28 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Thread Center</title>
   <link rel="stylesheet" href="styles/Style.css">
+  <link rel="stylesheet" href="styles/TopPage.css">
 </head>
 <body>
   
   <x-header/>
 
-  <div class="large-container">
-    <h2>Threads</h2>
+  <main>
+    <div class="large-container">
+      <h2>Threads</h2>
 
-    <div class="thread-card-container">
-      @foreach($threads as $thread)
-        <div class="thread-card">
-          <p class="title">{{ $thread->title}}</p>
-          <p class="created-at">{{ $thread->created_at }}</p>
-        </div>
-      @endforeach
+      <div class="thread-card-container">
+        @foreach($threads as $thread)
+          <div class="thread-card">
+            <p class="title">{{ $thread->title}}</p>
+            <p class="created-at">{{ $thread->created_at }}</p>
+          </div>
+        @endforeach
+      </div>
+      
     </div>
-    
-  </div>
-  
+
+  </main>
+
 </body>
 </html>
