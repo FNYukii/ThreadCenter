a317a40f20142e20942368de971a45e78c36dd59
Yu357 <forestgreen14689@gmail.com>
2022-08-27 03:25:39 +0900

    fix: Footerのロゴを修正

--- resources/views/Components/footer.blade.php
+++ resources/views/Components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="large-container">
-        <a href="/" class="logo">Thread Hub</a>
+        <a href="/" class="logo">Thread Center</a>
         <p>Discussion for people.</p>
         
         <p class="copyright">Copyright Yu357 2022.</p>
