5d4587e06ef9639122a84437b5741ac205a1dbfa
Yu357 <forestgreen14689@gmail.com>
2022-08-26 17:25:53 +0900

    feat: aタグのリンク先を設定

--- resources/views/Pages/TopPage.blade.php
+++ resources/views/Pages/TopPage.blade.php
@@ -19,12 +19,12 @@
       <div class="thread-card-container">
         @foreach($threads as $thread)
           <div class="thread-card">
-            <a href="/"> </a>
+            <a href="/threads/{{ $thread->id }}"> </a>
             <p class="title">{{ $thread->title}}</p>
             <p class="created-at">{{ $thread->created_at }}</p>
           </div>
         @endforeach
-        
+
       </div>
       
     </div>
