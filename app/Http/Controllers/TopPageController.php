123fcf8d437b6bdb9bf1f02d6c502517f1673792
Yu357 <forestgreen14689@gmail.com>
2022-08-26 16:57:40 +0900

    feat: Bladeに変数threadsを渡すようにした

--- app/Http/Controllers/TopPageController.php
+++ app/Http/Controllers/TopPageController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Thread;
 use Illuminate\Http\Request;
 
 class TopPageController extends Controller
 {
     public function __invoke(Request $request)
     {
-        return view('Pages.TopPage');
+        // 全てのThreadを取得
+        $threads = Thread::orderBy('created_at', 'DESC')->get();
+
+        // Bladeを返す
+        return view('Pages.TopPage')
+        ->with('threads', $threads);
     }
 }
