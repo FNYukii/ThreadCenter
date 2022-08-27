69e6b44f3eb73de65977479c15debc16fde259ed
Yu357 <forestgreen14689@gmail.com>
2022-08-27 14:23:38 +0900

    fix: 背景色が白色の領域が現れる不具合を修正した

--- resources/views/Pages/TopPage.blade.php
+++ resources/views/Pages/TopPage.blade.php
@@ -9,53 +9,56 @@
   <link rel="stylesheet" href="styles/TopPage.css">
 </head>
 <body>
+
+  <div class="root">
+    <x-header/>
   
-  <x-header/>
-
-  <main>
-    <div class="large-container">
-
-      <!-- Add Thread Modal -->
-      <div class="modal-shadow" id="modalShadow"></div>
-      <div class="modal" id="modal">
-        <h3>New thread</h3>
-        <form action="{{ route('createThread') }}" method="post">
-          @csrf
-          <input placeholder="Title" name="title" id="titleInput"/>
-
-          <div class="button-container">
-            <button type="submit" id="submitButton" disabled>Create</button>
-          </div>
-        </form>
-      </div>
-
-      <!-- Title Bar -->
-      <div class="title-bar">
-        <h2>Threads</h2>
-
-        <div>
-          <button id="openButton">New Thread</button>
+    <main>
+      <div class="large-container">
+  
+        <!-- Add Thread Modal -->
+        <div class="modal-shadow" id="modalShadow"></div>
+        <div class="modal" id="modal">
+          <h3>New thread</h3>
+          <form action="{{ route('createThread') }}" method="post">
+            @csrf
+            <input placeholder="Title" name="title" id="titleInput"/>
+  
+            <div class="button-container">
+              <button type="submit" id="submitButton" disabled>Create</button>
+            </div>
+          </form>
         </div>
-      </div>
-
-      <!-- Thread Card Container -->
-      <div class="thread-card-container">
-        @foreach($threads as $thread)
-          <div class="thread-card">
-            <a href="/threads/{{ $thread->id }}"> </a>
-            <p class="title">{{ $thread->title}}</p>
-            <p class="created-at">{{ $thread->created_at }}</p>
+  
+        <!-- Title Bar -->
+        <div class="title-bar">
+          <h2>Threads</h2>
+  
+          <div>
+            <button id="openButton">New Thread</button>
           </div>
-        @endforeach
-
-        <div class="invisible-thread-card"></div>
+        </div>
+  
+        <!-- Thread Card Container -->
+        <div class="thread-card-container">
+          @foreach($threads as $thread)
+            <div class="thread-card">
+              <a href="/threads/{{ $thread->id }}"> </a>
+              <p class="title">{{ $thread->title}}</p>
+              <p class="created-at">{{ $thread->created_at }}</p>
+            </div>
+          @endforeach
+  
+          <div class="invisible-thread-card"></div>
+        </div>
+        
       </div>
-      
-    </div>
+  
+    </main>
+  
+    <x-footer/>
 
-  </main>
-
-  <x-footer/>
+  </div>
 
   <script src="scripts/Script.js"></script>
   <script src="scripts/CheckThreadValue.js"></script>
