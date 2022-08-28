0529f12c325f1f2765f9d96b1acfa7de623b6f4c
Yu357 <forestgreen14689@gmail.com>
2022-08-28 21:23:36 +0900

    clean: コードを読みやすくした

--- resources/views/Components/Header.blade.php
+++ resources/views/Components/Header.blade.php
@@ -4,6 +4,13 @@
 
         <ul>
             @auth
+            <li>
+                <form method="post" action="{{ route('logout') }}">
+                    @csrf
+                    <button onclick="event.preventDefault(); this.closest('form').submit();">Sign out</button>
+                </form>
+            </li>
+            
             @else
             <li>
                 <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
@@ -11,15 +18,6 @@
 
             <li>
                 <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-            </li>
-            @endauth
-
-            @auth
-            <li>
-                <form method="post" action="{{ route('logout') }}">
-                    @csrf
-                    <button onclick="event.preventDefault(); this.closest('form').submit();">Sign out</button>
-                </form>
             </li>
             @endauth
         </ul>
