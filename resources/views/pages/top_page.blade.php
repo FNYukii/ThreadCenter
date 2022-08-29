0fdc4cef5d8deaaaf0fb3cd9b2b0f3c493031d8d
Yu357 <forestgreen14689@gmail.com>
2022-08-30 04:22:57 +0900

    fix: cssファイルを読み込むlinkタブを修正

--- resources/views/pages/top_page.blade.php
+++ resources/views/pages/top_page.blade.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Thread Center</title>
-    <link rel="stylesheet" href="styles/Style.css">
-    <link rel="stylesheet" href="styles/TopPage.css">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/top_page.css">
 </head>
 
 <body>
